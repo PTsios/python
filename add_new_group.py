6ebee4baaf437d769b4b52e22cb781eb2d56381f
PTsios <pavel.tsios@devhouse.pro>
2017-10-15 23:51:08 +0300

    code refactoring

--- add_new_group.py
+++ add_new_group.py
@@ -14,10 +14,14 @@ class add_new_group(unittest.TestCase):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
-    def test_add_new_group(self):
-        success = True
-        wd = self.wd
+
+    def open_home_page(self, wd):
+        # open home page
         wd.get("http://localhost/addressbook/index.php")
+
+
+    def login(self, wd):
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -25,7 +29,14 @@ class add_new_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+
+    def open_group_page(self, wd):
+        # open group page
         wd.find_element_by_link_text("groups").click()
+
+
+    def creation_new_group(self, wd):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -37,10 +48,29 @@ class add_new_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("33333")
         wd.find_element_by_name("submit").click()
+
+
+    def return_to_group_page(self, wd):
+        # return to group page
         wd.find_element_by_link_text("group page").click()
+
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
+
+    def test_add_new_group(self):
+        success = True
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.creation_new_group(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
         self.assertTrue(success)
-    
+
+
     def tearDown(self):
         self.wd.quit()
 
