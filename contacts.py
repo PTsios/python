538d5294b1b9dc3b4b38a656323809c1f9e062df
PTsios <pavel.tsios@devhouse.pro>
2017-10-16 13:50:40 +0300

    adding new contacts file

---
+++ contacts.py
@@ -0,0 +1,11 @@
+class Contact:
+    def __init__(self, firstname, middlename, nickname, tittle, company, address, home, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.tittle = tittle
+        self.company = company
+        self.address = address
+        self.home = home
+        self.email = email
+         
