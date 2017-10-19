69063ccf780d53aa0d283cfbf920b83d0a1eae10
PTsios <pavel.tsios@devhouse.pro>
2017-10-19 14:42:51 +0300

    code refactoring

--- add_new_group.py
+++ add_new_group.py
@@ -1,22 +1,17 @@
 # -*- coding: utf-8 -*-
 import pytest
+
 from group import Group
 from application import Application
 
 
 @pytest.fixture
-def app():
+def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
-class add_new_group(app):
-    def setUp(self):
-        self.app = Application()
-
 def test_add_new_group(app):
-    success = True
     app.login(username="admin", password="secret")
     app.creation_new_group(Group(name="1111111", header="22222", footer="33333"))
     app.logout()
-    app.assertTrue(success)
