1e414c6a404fe03fa75758362dcc73fde0d0a48d
PTsios <pavel.tsios@devhouse.pro>
2017-10-24 12:27:19 +0300

    group -> refactoring

--- fixture/application.py
+++ fixture/application.py
@@ -1,22 +1,18 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 class Application:
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
 
     def open_home_page(self):
         # open home page
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
-
-
-    def open_group_page(self):
-        # open group page
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
 
 
     def adding_new_contact(self, contacts):
@@ -52,34 +48,6 @@ class Application:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
 
-    def creation_new_group(self, group):
-        wd = self.wd
-        self.open_group_page()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("%s" % group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("%s" % group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
-
-
-    def return_to_group_page(self):
-        # return to group page
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-
-
-
-
-    def test_add_new_group(self):
-        success = True
 
     def destroy(self):
         self.wd.quit()
--- test/add_new_group.py
+++ test/add_new_group.py
@@ -5,6 +5,7 @@ from fixture.application import Application
 from model.group import Group
 
 
+
 @pytest.fixture
 def app(request):
     fixture = Application()
@@ -14,11 +15,11 @@ def app(request):
 
 def test_add_new_group(app):
     app.session.login(username="admin", password="secret")
-    app.creation_new_group(Group(name="1111111", header="22222", footer="33333"))
+    app.group.create_new(Group(name="1111111", header="22222", footer="33333"))
     app.session.logout()
 
 
 def test_add_new_blank_group(app):
     app.session.login(username="admin", password="secret")
-    app.creation_new_group(Group(name="", header="", footer=""))
+    app.group.create_new(Group(name="", header="", footer=""))
     app.session.logout()
