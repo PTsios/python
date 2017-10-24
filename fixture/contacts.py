de6f25d506d3291bafac5d750eb2bc3ec80360bf
PTsios <pavel.tsios@devhouse.pro>
2017-10-24 12:34:49 +0300

    added supporting class -> ContactsHelper

---
+++ fixture/contacts.py
@@ -0,0 +1,38 @@
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def adding_new_contact(self, contacts):
+        # add new contact
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("%s" % contacts.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys("%s" % contacts.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").send_keys("%s" % contacts.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys("%s" % contacts.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys("%s" % contacts.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys("%s" % contacts.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("%s" % contacts.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("%s" % contacts.home)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("%s" % contacts.email)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_new_blank_contact(app):
     app.session.login( username="admin", password="secret")
-    app.adding_new_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="",company="", address="",home="",email=""))
+    app.contacts.adding_new_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="",company="", address="",home="",email=""))
     app.session.logout()
 
 
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.adding_new_contact(Contact(firstname="Pavel", middlename="Notallowed", lastname="Tsios", nickname="raynalds", title="QA", company="devhouse", address="Russia. Stavropol", home="9175265472", email="pavel.tsios@devhouese.pro"))
+    app.contacts.adding_new_contact(Contact(firstname="Pavel", middlename="Notallowed", lastname="Tsios", nickname="raynalds", title="QA", company="devhouse", address="Russia. Stavropol", home="9175265472", email="pavel.tsios@devhouese.pro"))
     app.session.logout()
