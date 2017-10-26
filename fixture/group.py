3bdf985590478d6e2e49075fb1a461224a7e12cb
PTsios <pavel.tsios@devhouse.pro>
2017-10-26 13:03:18 +0300

    added deleting group validation

--- fixture/group.py
+++ fixture/group.py
@@ -76,3 +76,10 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
 
 
+    def count(self):
+        wd = self.app.wd
+        self.open_group_page()
+        return len(wd.find_elements_by_link_text("selected[]"))
+
+
+
