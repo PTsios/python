4916e6ae723b0a9011e738c645f3713e387de4ab
PTsios <pavel.tsios@devhouse.pro>
2017-10-30 17:56:14 +0300

    delete useless files

--- test/test_modificate_contact.py
+++
@@ -1,4 +0,0 @@
-from model.contact_modification import Contact_modification
-
-def test_contact_modification(app):
-    app.contacts.contact_modify(Contact_modification("NuPavel", "neznayu", "NuTsios", "Nuraynalds", "NuQA", "Nudevhouse", "Kavkaz", "1111111", "utreoos@devhouese.pro"))
