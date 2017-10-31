11fa2ffb31123433d92c6687c0a45b5f68e1513f
PTsios <pavel.tsios@devhouse.pro>
2017-10-31 19:07:51 +0300

    added validation
    if there if no contacts to modify - new will be added

--- test/test_modify_first_contact.py
+++ test/test_modify_first_contact.py
@@ -2,4 +2,6 @@ from model.contacts import Contact
 
 
 def test_modify_first_contact_firstname(app):
+    if app.contacts.edit_count() == 0:
+        app.contacts.adding_new_contact(Contact(firstname="Pavel", middlename="Notallowed", lastname="Tsios", nickname="raynalds", title="QA", company="devhouse", address="Russia. Stavropol", home="9175265472", email="pavel.tsios@devhouese.pro"))
     app.contacts.modify_first_contact(Contact(firstname="YOYOYOYOYOYOOY"))
