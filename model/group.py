f6aff723ba0d74094504a053fc20c4ad479f124c
PTsios <pavel.tsios@devhouse.pro>
2017-11-02 14:18:04 +0300

    added assert for new group

--- model/group.py
+++ model/group.py
@@ -1,11 +1,14 @@
+from sys import maxsize
+
+
 class Group:
 
 
     def __init__(self, name=None, header=None, footer=None, id = None):
-        self.id = id
         self.name = name
         self.header = header
         self.footer = footer
+        self.id = id
 
 
     def __repr__(self):
@@ -13,4 +16,11 @@ class Group:
 
 
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
