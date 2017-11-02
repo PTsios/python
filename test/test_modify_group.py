06b98b7942594badc01f3ffdd044d119c9227cc6
PTsios <pavel.tsios@devhouse.pro>
2017-11-02 14:30:24 +0300

    assert for modify group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,21 +9,34 @@ def checking_if_group_exists(app): #if there is no group - new will be created
 def test_modify__first_group_name(app):
     old_groups = app.group.get_group_list()
     checking_if_group_exists(app)
-    app.group.modify_first_group(Group(name="44444"))
+    group = Group(name="44444")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-
-def test_modify_first_group_header(app):
-    old_groups = app.group.get_group_list()
-    checking_if_group_exists(app)
-    app.group.modify_first_group(Group(header="55555"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_first_group_footer(app):
-    old_groups = app.group.get_group_list()
-    checking_if_group_exists(app)
-    app.group.modify_first_group(Group(footer="777"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+#def test_modify_first_group_header(app):
+#    old_groups = app.group.get_group_list()
+#    checking_if_group_exists(app)
+#    group = Group(header="55555")
+#    group.id = old_groups[0].id
+#    app.group.modify_first_group(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
+#    old_groups[0] = group
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+# def test_modify_first_group_footer(app):
+#    old_groups = app.group.get_group_list()
+#    checking_if_group_exists(app)
+#    group = Group(footer="777")
+#   group.id = old_groups[0].id
+#    app.group.modify_first_group(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
+#    old_groups[0] = group
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
