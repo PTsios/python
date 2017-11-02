d2a954ea627d0aa8800f40807ee734fb1656081f
PTsios <pavel.tsios@devhouse.pro>
2017-11-02 13:38:33 +0300

    added avlidation for group counts

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -3,8 +3,13 @@ from model.group import Group
 
 
 def test_add_new_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_new(Group(name="1111111", header="22222", footer="33333"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_new_blank_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_new(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -7,15 +7,23 @@ def checking_if_group_exists(app): #if there is no group - new will be created
 
 
 def test_modify__first_group_name(app):
+    old_groups = app.group.get_group_list()
     checking_if_group_exists(app)
     app.group.modify_first_group(Group(name="44444"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_first_group_header(app):
+    old_groups = app.group.get_group_list()
     checking_if_group_exists(app)
     app.group.modify_first_group(Group(header="55555"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_first_group_footer(app):
+    old_groups = app.group.get_group_list()
     checking_if_group_exists(app)
     app.group.modify_first_group(Group(footer="777"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
